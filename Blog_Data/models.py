6d4b53289a591f0a7326c5b070b26e28ee3914fc
Taha Mahmood <M.TahaMahmood27@gmail.com>
2023-01-19 22:52:24 +0500

    backend improved

--- Blog_Data/models.py
+++ Blog_Data/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Blog(models.Model):
+    topic_Heading = models.CharField(max_length=200)
+    auther = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField(null=False)
+    updated = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    
+
