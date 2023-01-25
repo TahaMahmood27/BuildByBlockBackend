d31f28633ccdad754e4522f5a4ac8b5236b5b2ed
TahaMahmood27 <M.TahaMahmood27@gmail.com>
2023-01-25 22:17:18 +0500

    some authentication functionality added

--- Blog_Data/api/views.py
+++ Blog_Data/api/views.py
@@ -2,14 +2,24 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from Blog_Data.models import Blog
 from .serializers import BlogSerializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-@api_view(['GET'])
-def getroutes(request):
-    routes = [
-        'GET /api',
-        "GET /api/rooms"
-    ]
-    return Response(routes)
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['name'] = user.username
+        # ...
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+
 
 @api_view(['GET'])
 def getBlog(request):
